423b1a7d03979ebe5f8e38343dde6701113575d9
Maslooo <Maslennikov.d.v1@gmail.com>
2024-11-01 15:00:28 +0300

    Update Произвольное число параметровHW.py
    Добавил проверку, когда word в root_word
    Также немного облегчил программу

--- Произвольное число параметровHW.py
+++ Произвольное число параметровHW.py
@@ -1,11 +1,11 @@
-def single_root_words(root_word, *other_words):
+def single_root_words(root_word,*other_words):
     same_words = []
     for word in other_words:
-        if root_word.lower() in word.lower():
+        if root_word in word:
+            same_words.append(word)
+        if word in root_word:
             same_words.append(word)
     return same_words
 
-result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-print(result1)
-print(result2)
+result = single_root_words("cool", "cooler", "coolest", "ice", "mountain", "uncool", "ool")
+print(result)
